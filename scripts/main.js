6cc50df3eefcce5e64e907177f77d310a84bf42b
dimitriwalters <me@dimi.io>
2015-08-14 15:27:16 -0700

    Epic stuff done

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,7 @@ function readURL(input) {
             var reader = new FileReader();
 
             reader.onload = function (e) {
-                $('#blah')
-                    .attr('src', e.target.result)
-                    .width(150)
-                    .height(200);
+                $('img').attr('src', e.target.result);
             };
 
             reader.readAsDataURL(input.files[0]);
